b91adb3ba07f32c517ae7be310092bb0d537f48e
jeffe <jeffe@JEFFERSON-TAMBACO.espe.local>
2017-07-14 20:13:24 -0500

    Creacion de la clase IndexBean para alumno modificar el pom con la dependencia

--- Educat05-web/src/main/java/espe/edu/ec/educat/web/IndexBean.java
+++ Educat05-web/src/main/java/espe/edu/ec/educat/web/IndexBean.java
@@ -5,10 +5,29 @@
  */
 package espe.edu.ec.educat.web;
 
-/**
- *
- * @author jeffe
- */
+import espe.edu.ec.educat.model.Alumno;
+import espe.edu.ec.educat.service.AlumnoService;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
 public class IndexBean {
-    
+
+    @EJB
+    private AlumnoService alumnoService;
+    private List<Alumno> alumnos;
+
+    @PostConstruct
+    public void inicio() {
+        this.alumnos = this.alumnoService.obtenerAlumno();
+    }
+
+    public List<Alumno> getAlumnos() {
+        return alumnos;
+    }
+
 }
